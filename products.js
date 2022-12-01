d7ff6a241c8a0fe0d3947bdf53c7a3c2379666af
willyder <willyder20@gmail.com>
2022-12-01 20:32:30 -0300

    projeto de base

---
+++ products.js
@@ -0,0 +1,34 @@
+let scanf = require('scanf');
+
+const produtos = [
+    {
+        nome: "Notebook",
+        categoria: "Eletrônico",
+        valor: 1999.99
+    },
+    {
+        nome: "Impressora",
+        categoria: "Eletrônico",
+        valor: 999.99
+    },
+    {
+        nome: "Caneta BIC",
+        categoria: "Escolar",
+        valor: 0.50
+    },
+    {
+        nome: "Lapiseira Pentel",
+        categoria: "Escolar",
+        valor: 7.50
+    },
+]
+
+
+function listar() {
+    produtos.forEach(p => {
+        console.log(p)
+    })
+}
+
+
+
